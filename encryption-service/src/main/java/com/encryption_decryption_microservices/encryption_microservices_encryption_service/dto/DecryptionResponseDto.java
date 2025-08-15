596acc8438822e1e25a30e853ca0bcb0d33a71a3
Sushant Paudel <sushantpaudel77@gmail.com>
2025-08-15 20:38:06 +0545

    feat: add dto's

---
+++ encryption-service/src/main/java/com/encryption_decryption_microservices/encryption_microservices_encryption_service/dto/DecryptionResponseDto.java
@@ -0,0 +1,13 @@
+package com.encryption_decryption_microservices.encryption_microservices_encryption_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DecryptionResponseDto {
+    private String decryptedText;
+    private String algorithm;
+}
---
+++ encryption-service/src/main/java/com/encryption_decryption_microservices/encryption_microservices_encryption_service/dto/EncryptionRequestDto.java
@@ -0,0 +1,14 @@
+package com.encryption_decryption_microservices.encryption_microservices_encryption_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EncryptionRequestDto {
+    private Long userId;
+    private String text;
+    private String algorithm; // "AES", "BASE64"
+}
