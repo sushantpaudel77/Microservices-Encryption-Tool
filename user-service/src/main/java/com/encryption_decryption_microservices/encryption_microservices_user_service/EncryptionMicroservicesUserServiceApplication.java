1cc5c51058ebf658cdb46c8f674bdbb31150db7f
Sushant Paudel <sushantpaudel77@gmail.com>
2025-08-16 20:40:48 +0545

    feat: add missing annotation in main class

--- user-service/src/main/java/com/encryption_decryption_microservices/encryption_microservices_user_service/EncryptionMicroservicesUserServiceApplication.java
+++ user-service/src/main/java/com/encryption_decryption_microservices/encryption_microservices_user_service/EncryptionMicroservicesUserServiceApplication.java
@@ -2,12 +2,13 @@ package com.encryption_decryption_microservices.encryption_microservices_user_se
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class EncryptionMicroservicesUserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EncryptionMicroservicesUserServiceApplication.class, args);
 	}
-
 }
