e27d054c49c7a9cf2b47be213c252b10f821120e
Sushant Paudel <sushantpaudel77@gmail.com>
2025-08-16 21:40:11 +0545

    feat: configure all flaws

--- encryption-service/src/main/java/com/encryption_decryption_microservices/encryption_microservices_encryption_service/EncryptionMicroservicesEncryptionServiceApplication.java
+++ encryption-service/src/main/java/com/encryption_decryption_microservices/encryption_microservices_encryption_service/EncryptionMicroservicesEncryptionServiceApplication.java
@@ -2,8 +2,10 @@ package com.encryption_decryption_microservices.encryption_microservices_encrypt
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
+@EnableFeignClients
 @EnableMongoAuditing
 @SpringBootApplication
 public class EncryptionMicroservicesEncryptionServiceApplication {
