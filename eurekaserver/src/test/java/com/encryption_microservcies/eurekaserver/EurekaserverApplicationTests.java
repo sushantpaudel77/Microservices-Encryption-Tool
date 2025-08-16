5b0965fbe02fee3dd8ad1dad319f89d51de37dbc
Sushant Paudel <sushantpaudel77@gmail.com>
2025-08-16 20:52:32 +0545

    feat: add eureka server

---
+++ eurekaserver/src/test/java/com/encryption_microservcies/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encryption_microservcies.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
