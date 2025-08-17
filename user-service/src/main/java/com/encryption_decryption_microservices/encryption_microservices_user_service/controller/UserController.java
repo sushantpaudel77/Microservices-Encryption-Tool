669a9f56ca1ccf4fd256dc5413a1bfc5d33cba89
Sushant Paudel <sushantpaudel77@gmail.com>
2025-08-17 10:22:51 +0545

    feat: add controller layer/user-service

---
+++ user-service/src/main/java/com/encryption_decryption_microservices/encryption_microservices_user_service/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.encryption_decryption_microservices.encryption_microservices_user_service.controller;
+
+import com.encryption_decryption_microservices.encryption_microservices_user_service.dto.CreateUserDto;
+import com.encryption_decryption_microservices.encryption_microservices_user_service.dto.UserDto;
+import com.encryption_decryption_microservices.encryption_microservices_user_service.dto.UserWithHistoryDto;
+import com.encryption_decryption_microservices.encryption_microservices_user_service.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(CreateUserDto createUserDto) {
+        var user = userService.createUser(createUserDto);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @GetMapping("/username/{username}")
+    public ResponseEntity<UserDto> getUserByUsername(@PathVariable String username) {
+        return ResponseEntity.ok(userService.getUserByUsername(username));
+    }
+
+   @GetMapping
+    public ResponseEntity<UserWithHistoryDto> getUserWithHistory(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserWithHistory(id));
+   }
+
+   @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
